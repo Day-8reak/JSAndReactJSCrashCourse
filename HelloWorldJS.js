90226fcccf4f264d61ea2aa0f64eff2aebad3a1e
Max <ninjamlgpro75@gmail.com>
2025-01-15 17:09:55 -0500

    First reactjs file
    Created my first reactjs project and deleted all of the code in app.jsx and it's dedicated style sheet file.
    
    Also deleted the images relating to the intitial project

---
+++ HelloWorldJS.js
@@ -0,0 +1,62 @@
+console.log("Hello JavaScript world"); // :ike a print statement
+
+
+//In-line Comment
+/* This is a 
+Mult-line comment
+*/
+
+value = 10;
+
+
+    // Data types:
+//Undefined, null, boolean, string, symbol (immutable primitive object), number, object
+var myName = "Max"; //Variables initialized with var, no need to specify datatype
+myName = 8; // Can change data types
+
+//Can also declare variables:
+
+let friendName = "Khoi is juicy"; // Only declared in the local scope
+const greeting = "Hello!"; // Is immutable, can't change anymore
+
+
+
+    //Storing values
+var a; //Declaring variable
+
+a = 7; // Assigning variable, can be done in the same line as declaring as shown before
+
+
+// If we ever print a variable onto the console that's yet
+// To have been assigned a value, then we get a null output
+
+
+
+    //Concatenation and addition 
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+//Adding and subtractingstrings and shit
+a = a + 5;
+b = b - 10;
+var c  = a * 10;
+var d = c / 5;
+c = c + " goat";
+//Incrementing and decremeing: (add/subtract 1)
+c++;
+c--;
+
+
+
+    //Declaration Vs Assignments
+var CAPSMATTER;
+capsMatter = 10; // This is a different variable from the previous declaration
+
+
+
+    //Math
+//Math is pretty much the same as in any other programming language
+ 
+
+
